f7bea897740cdf73152ba7c2d4b3c32712fc5511
rniranjan93 <33223840+rniranjan93@users.noreply.github.com>
2021-09-08 11:01:16 +0530

    Update configure.cmd

--- configure.cmd
+++ configure.cmd
@@ -1,20 +1,64 @@
-:: Copyright 2019 The TensorFlow Authors.  All Rights Reserved.
-::
-:: Licensed under the Apache License, Version 2.0 (the "License");
-:: you may not use this file except in compliance with the License.
-:: You may obtain a copy of the License at
-::
-::    http://www.apache.org/licenses/LICENSE-2.0
-::
-:: Unless required by applicable law or agreed to in writing, software
-:: distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-:: WARRANTIES OR CONDITIONS OF ANY KIND< either express or implied.  See the
-:: License for the specific language governing permissions and limitations under
-:: the License.
-
-@echo off
-
-set configure_dir=%~dp0
-set configure_dir=%configure_dir:~0,-1%
-python "%configure_dir%\configure.py" %* || ( exit /b )
-echo Configuration finished
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	int t;
+	long long n, l, r,index,a;
+	cin >> t;
+	while (t--)
+	{
+		cin >> n >> l >> r;
+		a = n*(n-1)+1 - l + 1;
+		if (a == 1)
+		{
+			cout << "1\n";
+			continue;
+		}
+		else
+		{
+			long long start = 1, end = n-1;
+			while (start <= end)
+			{
+				long long mid = (start + end) / 2;
+				if (a > mid * (mid + 1) + 1)
+				{
+					start = mid+1;
+				}
+				else
+				{
+					end = mid - 1;
+				}
+			}
+			index =start;
+		}
+		int i = n - index;
+		int j = i + 1 + ((index * (index + 1) + 1) - a) / 2;
+		while (l <= r)
+		{
+			if (i == n)
+			{
+				cout << "1 ";
+				break;
+			}
+			if (l % 2)
+			{
+				cout << i << ' ';
+			}
+			else
+			{
+				cout << j << ' ';
+				j++;
+			}
+			if (j > n)
+			{
+				i++;
+				j = i + 1;
+			}
+			l++;
+		}
+		cout << endl;
+	}
+	return 0;
+}
